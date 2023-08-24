1cd4dcc890f78461ae3318cabf5b81d88863ceb4
help-hope-live <tech@helphopelive.org>
2023-08-24 11:54:27 -0400

    'logo changes and intro'

--- components/Hero.vue
+++ components/Hero.vue
@@ -2,22 +2,22 @@
   <header class="py-12 px-4 sm:px-6 lg:px-8">
     <div class="relative mx-auto max-w-[37.5rem] pt-20 text-center pb-20">
       <div class="flex items-center justify-center space-x-3">
-        <img loading="lazy" src="/logo.png" alt="Logspot" class="h-20 w-20" />
-        <h1
-          class="text-4xl font-extrabold tracking-tight text-slate-700 sm:text-5xl"
-        >
-          logspot
-        </h1>
+        <img loading="lazy" src="/logo.png" alt="Logspot" class="h-250 w-250" />
+        
       </div>
-      <p class="mt-4 text-base leading-7 text-slate-600">
-        Logspot is an open source change log template made with Nuxt, Vue, and
-        Tailwindcss. Subscribe to our newsletter to get the latest updates or
-        follow us on
+      <h2
+          class="text-xl font-extrabold tracking-tight text-teal-600 sm:text-5xl"
+        >
+           Change Log
+        </h2>
+      <p class="mt-4 text-base leading-7 text-slate-700">
+        This changelog will represent all changes reflected within Jira Tickets. <br>
+        Check out this weekly for announcments and recent changes. 
         <a
           class="text-primary underline"
-          href="https://twitter.com/fayazara"
+          href="https://helphopelive.org"
           target="_blank"
-          >twitter</a
+          >HHL</a
         >.
       </p>
     </div>
